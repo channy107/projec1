8b7fc3536c926816a9ca153f3c772796a285af6e
user <hunchan12@gmail.com>
2018-10-25 14:48:48 +0900

    로또생성앱 model 기능추가+ test

--- lotto/tests.py
+++ lotto/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import GuessNumbers
+class GuessNumbersTestCase(TestCase):
 
-# Create your tests here.
+
+    def test_generate(self):
+        g = GuessNumbers(name='apple', text='pineapple')
+        g.generate()
+        print(g.update_date)
+        print(g.lottos)
+        self.assertTrue(len(g.lottos) > 20)
