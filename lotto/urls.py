2dde433c526074b6ae704ed1facc2870f85a0539
user <hunchan12@gmail.com>
2018-10-25 15:29:30 +0900

    로또생성앱 form table 추가

--- lotto/urls.py
+++ lotto/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('form', views.post),
     path('', views.index),
 ]
--- lotto/views.py
+++ lotto/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
 
 
 def index(request):
     lottos = GuessNumbers.objects.all()
     return render(request, 'lotto/index.html', {'lottos':lottos})
+
+def post(request):
+    form = PostForm()
+    return render(request, 'lotto/form.html',
+                  {'form': form})
