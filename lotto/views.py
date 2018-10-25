b53fd091282b548614607467c873a78272a7d270
user <hunchan12@gmail.com>
2018-10-25 15:48:17 +0900

    로또생성앱 post 추가

--- lotto/views.py
+++ lotto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -8,6 +8,19 @@ def index(request):
     return render(request, 'lotto/index.html', {'lottos':lottos})
 
 def post(request):
-    form = PostForm()
-    return render(request, 'lotto/form.html',
-                  {'form': form})
+    if request.method == 'GET':
+        form = PostForm()
+        return render(request, 'lotto/form.html',
+                      {'form': form})
+    else:  # POST
+        form = PostForm(request.POST)
+        if form.is_valid():
+            lotto = form.save(commit=False)
+            lotto.generate()
+            return redirect('/lotto')
+
+            # form = PostForm(request.POST)
+            # if form.is_valid():
+            #     form.save()
+
+
