1c202c032e0d92c5eaab98073b97b2dfa4ae7db3
user <hunchan12@gmail.com>
2018-10-26 13:37:31 +0900

    로또 경로 재 설정

--- lotto/urls.py
+++ lotto/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
     # 다른방식
     path('detail/<int:num>',
-         views.detail2),
+         views.detail2, name='detail2'),
     # 파라미터 방식
-    path('detail', views.detail),
+    path('detail', views.detail, name='detail'),
     path('form', views.post),
     path('', views.index),
-    path('join', views.join),
+    path('join', views.join, name='join'),
     path('id_check', views.id_check),
 ]
